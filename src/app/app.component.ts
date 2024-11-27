eb28dfa46a09cd355eff5bbe88b57d5bdbef1381
keneth/217 <kipyegonkeneth03@gmail.com>
2024-11-27 20:25:03 +0300

    events app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,34 +15,32 @@ import { Subscription } from "rxjs";
 })
 export class AppComponent {
   scannedResult: string = '';
+  isCameraAccessible: boolean = false;
   private scannerSubscription: Subscription | undefined;
 
   constructor(private scannerService: ScannerService) {}
 
   ngOnInit(): void {
-    // Request permission for the camera when the component is initialized
     this.requestCameraPermission();
   }
 
   ngOnDestroy(): void {
-    // Unsubscribe from scanner service if any active subscriptions
     if (this.scannerSubscription) {
       this.scannerSubscription.unsubscribe();
     }
   }
 
-  // Request camera permission and access the camera
   private requestCameraPermission(): void {
     navigator.mediaDevices.enumerateDevices()
       .then(devices => {
         const videoDevices = devices.filter(device => device.kind === 'videoinput');
         if (videoDevices.length === 0) {
+          this.isCameraAccessible = false;
           alert('No video input devices found.');
         } else {
-          // Use the first available video device or choose the one you prefer
           navigator.mediaDevices.getUserMedia({ video: { deviceId: videoDevices[0].deviceId } })
             .then((stream) => {
-              // Successfully got a stream (camera is accessible)
+              this.isCameraAccessible = true; // Camera is accessible
             })
             .catch((error) => {
               console.error('Error accessing the camera:', error);
@@ -52,14 +50,12 @@ export class AppComponent {
       })
       .catch((err) => {
         console.error('Error enumerating devices:', err);
+        this.isCameraAccessible = false;
       });
   }
 
-  // Handle QR code scan result
   onCodeResult(result: string): void {
     this.scannedResult = result;
-
-    // Call the service to scan QR code
     this.scannerSubscription = this.scannerService.scanQRCode(result).subscribe({
       next: (response) => {
         alert('QR Code scanned successfully!');
